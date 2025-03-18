f05b70638b1d0b08827cd376aae156a5567885b5
Rafa Amorielli <44513850+amorielli@users.noreply.github.com>
2025-03-18 19:31:01 -0300

    Limit FPS

--- main.py
+++ main.py
@@ -15,18 +15,17 @@ from constants import (
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        screen.fill("black")
         pygame.display.flip()
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
